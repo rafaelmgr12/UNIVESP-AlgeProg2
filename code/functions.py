761a54d67d51985bce4a8844b6106a76c2456332
Rafael <ribeirorafaelmatheus@gmail.com>
2020-10-23 17:32:51 -0300

    functions

--- code/functions.py
+++ code/functions.py
@@ -129,14 +129,49 @@ class Animal:
         self.ling = linguagem
     def fala(self):
         ' exibe uma sentença pelo animal'
-        print('IEu sou um {} e sei {}'.format(self.esp, self.ling))
+        print('Eu sou um {} e sei {}'.format(self.esp, self.ling))
     def __init__(self, espécie='animal', linguagem='emite sons '):
         'construtor '
         self.esp = espécie
         self.ling = linguagem
-#8.5 (dúvida)
+#8.5
+from random import shuffle
+class Baralho:
+  'representa um baralho de 52 cartas'
+  # valores e naipes são variáveis da classe Baralho
+  valores = {'2','3','4','5','6','7','8','9','10','J','Q','K','A'}
+  #naipes são 4 símbolos Unicode representando os 4 naipes
+  naipes = {'\u2660', '\u2661', '\u2662', '\u2663'}
+  def __init__(self,lista = None):
+    if lista != None:   # entra baralho fornecido
+      self.baralho = lista
+    else:# nenhum baralho incluído
+      'inicializa baralho de 52 cartas'
+      self.baralho = []       # baralho está inicialmente vazio 
+      for naipe in Baralho.naipes: # naipes e valores são Baralho
+        for valor in Baralho.valores: # variáveis da classe
+        # inclui Carta com certo valor e naipe no baralho
+          self.baralho.append(Carta(valor, naipe))
+  def distribuiCarta(self):
+    'distribui (remove e retorna) carta do topo do baralho'
+    return self.baralho.pop()
+  def shuffle(self):
+    'mistura o baralho'
+    shuffle(self.baralho)   
+    # outros métodos de Baralho
+  def __len__(self):
+    'retorna tamanho do baralho'
+    return len(self.baralho)
+  def __repr__(self):
+    'retorna representação de string canônica'
+    return 'Baralho ({})'.format(self.baralho)
+  def __eq__(self, outro):
+    '''retorna True se baralho tiver as
+         mesmas cartas na mesma ordem '''
+    return self.baralho == outro.baralho
 
-#8.6
+
+
 
 class Carta:
 
---
+++ code/turtlefunctions.py
@@ -0,0 +1,6 @@
+
+def jump(t,x, y):
+    'faz tartaruga saltar para coordenadas (x, y)'
+    t.penup()
+    t.goto(x,y)
+    t.pendown()
